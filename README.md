39b8ebee1b2ff5068c0063a994d5b52a656efe30
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-03 13:48:03 -0500

    basic html and css

---
+++ README.md
@@ -0,0 +1,6 @@
+
+JS sudo- code:
+create click events for each button
+push "clicked" numbers into input div
+Store repeatable operator functions in variables for repeat
+turn the string numbers into integers
--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log ('test');
+// Variables for operators
+
+let add = function add(a,b){
+  return a + b;
+}
+
+let sub = function sub(a,b){
+  return a - b;
+}
+
+let prod = function prod(a,b){
+  return a * b;
+}
+
+let div = function divide(a,b){
+  return a / b;
+}
