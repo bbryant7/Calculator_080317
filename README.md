eaf75b23a48bc35cad4563a5e4266a0208a4f16e
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-03 15:13:57 -0500

    all buttons tagged to event listeners and working properly.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 
 JS sudo- code:
 create click events for each button
+ - created click event for the class 'button' and used a loop to create a click button for every 'button' class
+ have click event output the correct 
 push "clicked" numbers into input div
 Store repeatable operator functions in variables for repeat
 turn the string numbers into integers
--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 // Variables for operators
+//
+// let add = function add(a,b){
+//   return a + b;
+// }
+//
+// let sub = function sub(a,b){
+//   return a - b;
+// }
+//
+// let prod = function prod(a,b){
+//   return a * b;
+// }
+//
+// let div = function divide(a,b){
+//   return a / b;
+// }
 
-let add = function add(a,b){
-  return a + b;
+let clickButton = document.getElementsByClassName("button");
+
+for (let i = 0; i < clickButton.length; i++) {
+  clickButton[i].addEventListener("click", myfunction);
+  function myfunction() {
+    console.log("number"); }
 }
 
-let sub = function sub(a,b){
-  return a - b;
-}
 
-let prod = function prod(a,b){
-  return a * b;
-}
 
-let div = function divide(a,b){
-  return a / b;
-}
+// document.getElmentsByClass("button[0]").addEventListener("click", function myfunction() { console.log("number"); });
+// document.getElmentsById("input").addEventListener("click", function myfunction() { console.log("number"); });
