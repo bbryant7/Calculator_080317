1f8b2410e1d927e079f92780668ec242e2367d99
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-04 14:45:43 -0500

    finished design

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 
 JS sudo- code:
-create click events for each button
+1. create click events for each button
  - created click event for the class 'button' and used a loop to create a click button for every 'button' class
- have click event output the correct 
-push "clicked" numbers into input div
-Store repeatable operator functions in variables for repeat
+2. have click event output the correct number or symbol
+3. push "clicked" numbers into input div
+4. Store repeatable operator functions in variables for repeat
 turn the string numbers into integers
+
+
+create cases for the submit button
+figure out how to give the parameter
+
+To Do for CSS:
+gradient and/or transparent buttons.
+transparent background for input
--- script.js
+++ script.js
@@ -1,30 +1,70 @@
 // Variables for operators
 //
-// let add = function add(a,b){
-//   return a + b;
-// }
-//
-// let sub = function sub(a,b){
-//   return a - b;
-// }
-//
-// let prod = function prod(a,b){
-//   return a * b;
-// }
-//
-// let div = function divide(a,b){
-//   return a / b;
-// }
+let add = function add(a,b){
+  return a + b;
+}
 
+let sub = function sub(a,b){
+  return a - b;
+}
+
+let prod = function prod(a,b){
+  return a * b;
+}
+
+let div = function divide(a,b){
+  return a / b;
+}
+
+// loop to add event listener to each button and have them print out corresponding symbol
 let clickButton = document.getElementsByClassName("button");
 
 for (let i = 0; i < clickButton.length; i++) {
   clickButton[i].addEventListener("click", myfunction);
   function myfunction() {
-    console.log("number"); }
+    let numbers = clickButton[i].value;
+    let test = `<p>${numbers}</p>`;
+
+    document.getElementById("input").innerHTML = test;
+
+
+    }
+
+
 }
 
 
 
-// document.getElmentsByClass("button[0]").addEventListener("click", function myfunction() { console.log("number"); });
-// document.getElmentsById("input").addEventListener("click", function myfunction() { console.log("number"); });
+
+// Event Listener for  submit button
+let clickSubmit = document.getElementById("submit");
+let operatorA = document.getElementById("add");
+
+let operatorS = document.getElementById("sub");
+
+let operatorP = document.getElementById("prod");
+
+let operatorD = document.getElementById("div");
+
+
+
+clickSubmit.addEventListener("click",calculate);
+function calculate(){
+  let answer = "ggg";
+ console.log(answer);
+}
+document.getElementById("input").innerHTML = answer;
+
+
+//   if (operator.value === "/"){
+//   return div;
+// }
+//   else if (operator.value === "*"){
+//   return prod;
+// }
+//   else if (operator[i].value === "+"){
+//   return add;
+// }
+//   else{
+//     return sub;
+//   }
