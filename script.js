c052b0be0eaef6184567dfec54f6859ffadadb2f
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-09 20:16:32 -0500

    add error for length of input field

--- script.js
+++ script.js
@@ -2,47 +2,51 @@
 // NUMBERS AND OPPERATORS buttons
 let input = document.getElementById("input");
 let clickButton = document.getElementsByClassName("button");
+// let clickOperator = document.getElementsByClassName("operator");
+let clickSubmit = document.getElementById("submit");
+let clickClear = document.getElementById("clear");
 let numbers = [];
-
-if (input.textContent.length > 10) {
-  console.log("yayyy!!! Length is too long");
-}
-console.log("the length is " + input.textContent.length);
-console.log(input.textContent);
+  // if (input.textContent.length < 10)
 
 for (let i = 0; i < clickButton.length; i++) {
-  clickButton[i].addEventListener("click", myfunction);
-
-  function myfunction() {
+  clickButton[i].addEventListener("click", function() {
     numbers += clickButton[i].value;
-    let output = `<p>${numbers}</p>`;
-    document.getElementById("input").innerHTML = output;
-  }
-
-  // SUBMIT BUTTON
-  let clickSubmit = document.getElementById("submit");
-  clickSubmit.addEventListener("click", calculate);
-
-  function calculate() {
-    let output = `<p>${eval(numbers)}</p>`;
-
-    document.getElementById("input").innerHTML = output;
-    let ouput = '';
+    // if (numbers[0] === ('/' || '*' || '+' || '-')) {
+    //   let output = `<p>Error</p>`;
+    //   document.getElementById("input").innerHTML = output;
+    // }
 
 
-  }
+    if (numbers.length < 20 ) {
+      let output = `<p>${numbers}</p>`;
+      document.getElementById("input").innerHTML = output;
+    }
+
+    else {
+      let output = `<p>The universe can not contain this equation!</p>`;
+      document.getElementById("input").innerHTML = output;
+    }
+
+  })
 }
 
+// SUBMIT BUTTON
+
+clickSubmit.addEventListener("click", function() {
+  let output = `<p>${eval(numbers)}</p>`;
+  document.getElementById("input").innerHTML = output;
+  let ouput = '';
+
+})
+
+
 // CLEAR BUTTON
-let clickClear = document.getElementById("clear");
 
-clickClear.addEventListener("click", clear);
-
-function clear() {
+clickClear.addEventListener("click", function() {
   numbers = "";
   let output = `<p>${numbers}</p>`;
   document.getElementById("input").innerHTML = output;
-}
+})
 
 
 // SPACE FACTS STARTS HERE
@@ -50,14 +54,12 @@ function clear() {
 let clickFacts = document.getElementById("space");
 let space = ["Neutron stars can spin at a rate of 600 rotations per second.", "If two pieces of the same type of metal touch in space, they will bond and be permanently stuck together.", "99% of our solar system's mass is the sun.", "Pluto is smaller than the Earth's moon.", "The Whirlpool Galaxy (M51) was the first celestial object identified as being spiral."];
 
-clickFacts.addEventListener("click", spaceFacts);
-
-function spaceFacts() {
+clickFacts.addEventListener("click", function() {
   let fact = space[Math.floor(Math.random() * space.length)];
   let output = `<p>${fact}</p>`;
   document.getElementById("input").innerHTML = output;
 
-}
+})
 
 
 // space facts:
@@ -69,59 +71,6 @@ function spaceFacts() {
 
 
 
-
-
-
-
-
-
-// -----------------------------
-// Variables for operators
-//
-// let add = function add(a,b){
-//   return a + b;
-// }
-//
-// let sub = function sub(a,b){
-//   return a - b;
-// }
-//
-// let prod = function prod(a,b){
-//   return a * b;
-// }
-//
-// let div = function divide(a,b){
-//   return a / b;
-// }
-
-
-
-
-
-// Event Listener for  submit button
-// let clickSubmit = document.getElementById("submit");
-// let operatorA = document.getElementById("add");
-//
-// let operatorS = document.getElementById("sub");
-//
-// let operatorP = document.getElementById("prod");
-//
-// let operatorD = document.getElementById("div");
-
-//   if (operator.value === "/"){
-//   return div;
-// }
-//   else if (operator.value === "*"){
-//   return prod;
-// }
-//   else if (operator[i].value === "+"){
-//   return add;
-// }
-//   else{
-//     return sub;
-//   }
-
-
 // Trying to create an if statement to create error if a none-number is put on numbers array first
 // if (numbers[0] === ('+' || '*'|| '/')) {
 //   let output = `<p>ERROR</p>`;
@@ -130,12 +79,7 @@ function spaceFacts() {
 //     console.log('output',output);
 // }
 //  else {
-//
-//
-//
 // }
-
-
 
 // option 2 with no eval:
 
